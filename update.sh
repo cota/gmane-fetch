6c2c7368795841d38ce46787aaa34b257031e327
Emilio G. Cota <cota@braap.org>
2011-06-18 20:08:26 -0400

    update: exit if .listname isn't present
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- update.sh
+++ update.sh
@@ -2,12 +2,20 @@
 
 set -e
 
+LISTNAME_FILE=.listname
+
+if [ ! -e $LISTNAME_FILE ]
+then
+	echo "Cannot find .listname file, exiting."
+	exit 1
+fi
+
 if [ ! -e [0-9]* ]
 then
 	touch 0
 fi
 
-LISTNAME=$(cat .listname | sed '/^$/d' | sed 1q)
+LISTNAME=$(cat $LISTNAME_FILE | sed '/^$/d' | sed 1q)
 
 ./gmane-update.sh $LISTNAME [0-9]*
 exit 0
